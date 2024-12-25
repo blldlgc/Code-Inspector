ea65e539b5d2971eb58b961363dd3df0383015c4
Btlsn <154734485+Btlsn@users.noreply.github.com>
2024-12-25 14:20:21 +0300

    .

--- CodeInspectorBackend/src/main/java/com/codeinspector/backend/dto/CodeAnalysisResult.java
+++ CodeInspectorBackend/src/main/java/com/codeinspector/backend/dto/CodeAnalysisResult.java
@@ -3,30 +3,15 @@ package com.codeinspector.backend.dto;
 import java.util.List;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CodeAnalysisResult {
-    private final Map<String, Double> smellScores;
-    private final Map<String, List<String>> smellDetails;
-    private final double overallScore;
-
-    public CodeAnalysisResult(Map<String, Double> smellScores, 
-                            Map<String, List<String>> smellDetails, 
-                            double overallScore) {
-        this.smellScores = smellScores;
-        this.smellDetails = smellDetails;
-        this.overallScore = overallScore;
-    }
-
-    public Map<String, Double> getSmellScores() {
-        return smellScores;
-    }
-
-    public Map<String, List<String>> getSmellDetails() {
-        return smellDetails;
-    }
-
-    public double getOverallScore() {
-        return overallScore;
-    }
+    private Map<String, Double> smellScores;
+    private Map<String, List<String>> smellDetails;
+    private double overallScore;
 }
