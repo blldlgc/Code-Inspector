cab01bb653d0bc1ad480e65a8bfc953dc8321311
blldlgc <119047452+blldlgc@users.noreply.github.com>
2025-09-03 18:41:35 +0300

    Add PostgreSQL integration and User entity
    Integrated PostgreSQL with Spring Boot by adding JPA and PostgreSQL dependencies, configuring database properties, and updating docker-compose to include a PostgreSQL service. Introduced User entity and UserRepository for user data management.

---
+++ CodeInspectorBackend/src/main/java/com/codeinspector/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codeinspector.backend.repository;
+
+import com.codeinspector.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
